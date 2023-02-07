1cd0b8034916fb71688e50c310ea1d811228ba9c
iiwe <ondarao@mail.ru>
2023-02-07 16:08:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tyva/Dictionary.java
@@ -0,0 +1,14 @@
+package com.example.tyva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dictionary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+}
---
+++ app/src/main/java/com/example/tyva/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tyva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void About(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
+    }
+    public void Dictionary(View view) {
+        Intent intent = new Intent(this, Dictionary.class);
+        startActivity(intent);
+    }
+    public void Mystery(View view) {
+        Intent intent = new Intent(this, Mystery.class);
+        startActivity(intent);
+    }
+    public void PoI(View view) {
+        Intent intent = new Intent(this, PoI.class);
+        startActivity(intent);
+    }
+}
