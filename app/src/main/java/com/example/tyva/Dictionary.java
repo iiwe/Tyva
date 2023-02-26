a7ac768c9ce6f27c0831e08a4760e0cbde970e0e
iiwe <ondarao@mail.ru>
2023-02-26 18:07:42 +0700

    изменен дизайн

--- app/src/main/java/com/example/tyva/Dictionary.java
+++ app/src/main/java/com/example/tyva/Dictionary.java
@@ -11,24 +11,25 @@ import android.widget.TextView;
 
 public class Dictionary extends AppCompatActivity {
     String[] russian_words = {"Здравствуйте!", "Да", "Нет", "Доброе утро!", "Добрый день!",
-            "Добрый вечер!", "Да", "Нет", "Меня зовут…, а вас?","До свидания!", "До скорого!", "Я турист",
+            "Добрый вечер!", "Меня зовут…, а вас?","До свидания!", "До скорого!", "Я турист",
             "Я вас не понимаю", "Cпасибо!", "Большое спасибо!", "Извините", "Можно войти?", "Сколько?",
             "Что вы хотите?", "Покажите на карте", "Как называется эта улица?", "Мы хотим купить сувениры",
             "Как проехать до…?", "Этот автобус идет до…?", "Разрешите позвонить?", "Где можно купить?",
             "Нам нужен...", "Где можно покушать?", "Очень вкусно!"};
-    String[] tyva_words = {"Экии!", "Ийе", "Чок", "Эртенгиниӊ \n мендизи-биле!", "Хүндүскүнүӊ мендизи-биле!",
+    String[] tyva_words = {"Экии!", "Ийе", "Чок", "Эртенгиниӊ мендизи-биле!", "Хүндүскүнүӊ мендизи-биле!",
             "Кежээкиниӊ мендизи-биле!", "Мени… дээр, а силерниӊ адыӊар кым?","Байырлыг!", "Ужурашкыже!",
             "Мен турист мен", "Мен силерни билбейн тур мен", "Четтирдим!", "Уулу-биле четтирдим!",
             "Буруулуг болдум", "Кирип болур бе?", "Каш?", "Силерге чүү херек?", "Картага айтып бериңерем",
             "Бо кудумчунуң адын чүү дээр?", "Бис сувенирлер садып алыксап тур бис", ".. чедир канчап чедип алыр?",
             "Бо автобус … чедер бе?", "Телефон долгаптайн, болур бе?", "Кайыын садып ап болур?",
             "Биске херек...", "Мында ... бар бе", "Чемненир", "Кая чемненип болур?", "Дендии амданныг!"};
-    int[] colors = new int[1];
+    int[] colors = new int[2];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
         colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#359967CC");
         LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
         LayoutInflater ltInflater = getLayoutInflater();
         for (int i = 0; i < russian_words.length; i++){
@@ -38,7 +39,7 @@ public class Dictionary extends AppCompatActivity {
             TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
             tvPosition.setText(tyva_words[i]);
             item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
-            item.setBackgroundColor(colors[i % 1]);
+            item.setBackgroundColor(colors[i % 2]);
             linLayout.addView(item);
 
         }
