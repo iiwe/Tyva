211b225eb2c86d8848d745b1f5dace4c30727e83
iiwe <ondarao@mail.ru>
2023-04-09 18:10:34 +0700

    доработан разговорник, обои

--- app/src/main/java/com/example/tyva/About.java
+++ app/src/main/java/com/example/tyva/About.java
@@ -2,18 +2,22 @@ package com.example.tyva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.IOException;
 
 public class About extends AppCompatActivity {
     MediaPlayer mediaPlayer;
-    Button btn_stop, btn_play, btn_pause;
+    Button btn_stop, btn_pause;
+    ImageButton btn_play;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
