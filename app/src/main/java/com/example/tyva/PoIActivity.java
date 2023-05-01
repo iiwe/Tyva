8ad2bbbdff42ba5f065de1509eeeb47e3f836973
iiwe <ondarao@mail.ru>
2023-05-01 18:37:46 +0700

    проверка

--- app/src/main/java/com/example/tyva/PoIActivity.java
+++ app/src/main/java/com/example/tyva/PoIActivity.java
@@ -2,6 +2,8 @@ package com.example.tyva;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PoIActivity extends AppCompatActivity {
     ImageView imageView;
+    Button show_map;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +19,12 @@ public class PoIActivity extends AppCompatActivity {
         setContentView(R.layout.activity_poi_full);
         imageView = findViewById(R.id.foto);
         textView = findViewById(R.id.name);
+        show_map = findViewById(R.id.show_map);
         Intent intent = getIntent();
         PoIData myPoi = intent.getParcelableExtra("Selected");
+        if(myPoi.getCoords() == null){
+            show_map.setVisibility(View.INVISIBLE);
+        }
         textView.setText(myPoi.getName());
         imageView.setImageResource(myPoi.getFoto());
     }
